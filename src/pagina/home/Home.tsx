60962dc0127edee0ebcf7e82e18ee5f0dd510e7f
unknown <marcelobud@hotmail.com>
2023-02-27 22:00:03 -0300

    Home

---
+++ src/pagina/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import'./Home.css';
+
+function Home(){
+    return(
+        <>
+        <h1 className="titulo">Home</h1>
+        <img src="https://imgur.com/RNcVMcB.png" alt="Banner" width="1240" height="150" className="img"/>
+        </>
+    );
+}
+export default Home;
